e92b86590ac0ddf42a72f4c67dbdfafe208ee32a
Sergey <frostfreerun@gmail.com>
2023-03-28 10:17:24 +0300

    Added FAB with snackbar

--- app/src/main/java/com/shv/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/shv/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -3,15 +3,46 @@ package com.shv.vknewsclient.ui.theme
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    val snackBarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackBarHostState.showSnackbar(
+                            message = "This is snackbar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
         bottomBar = {
             BottomNavigation() {
                 Log.d("COMPOSE_TEST", "BottomNavigation")
