e2da5367c1e9b862496cd1401ef0170440e8918a
Sergey <frostfreerun@gmail.com>
2023-04-07 13:47:31 +0300

    Used ActivityResultApi for compose

--- app/src/main/java/com/shv/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/shv/vknewsclient/MainActivity.kt
@@ -3,16 +3,15 @@ package com.shv.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.shv.vknewsclient.ui.theme.MainScreen
+import com.shv.vknewsclient.ui.theme.ActivityResultTest
 import com.shv.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VkNewsClientTheme() {
-                MainScreen()
+            VkNewsClientTheme {
+                ActivityResultTest()
             }
         }
     }
